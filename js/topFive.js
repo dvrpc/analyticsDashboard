eaa5e90c8b105fc0f7333797dcb2304201b219f8
Marc Molta <MMolta@dvrpc.org>
2017-12-15 10:11:42 -0500

    CORS request issue resolved

--- js/topFive.js
+++ js/topFive.js
@@ -1,22 +1,54 @@
 // get a hold of the top five cards
 const topFive = document.querySelectorAll('.top-page-box')
-console.log('the top five boxes ', topFive)
 
-const today = new Date();
-console.log('today ', today)
+// paramaters to include in the url
+const startDate = new Date().toISOString().split('T')[0]
+const endDate = 'tbd'
+const dimensions = 'ga:pagePath'
+const metrics = 'ga:pageviews,ga:sessions,ga:avgTimeOnPage'
 
-const options = {
-	startDate: today,
-	endDate: today,
-	dimension: ''
+const topPages = `http://intranet.dvrpc.org/google/analytics?startDate=${startDate}&endDate=2017-12-15&dimension=${dimensions}&metric=${metrics}&sortByMetric=true`
+
+function createCORSRequest(method, url) {
+	const xhr = new XMLHttpRequest()
+
+	// handle xmlhttprequest2 objects which have a withCredentials property (most use cases)
+	if('withCredentials' in xhr){
+		xhr.open(method, url, true)
+
+	// IE uses XDomainRequest objects to make CORS requests
+	}else if(typeof XDomainRequest != 'undefined'){
+		xhr = new XDomainRequest()
+		xhr.open(method, url)
+
+	// if CORS isn't supported by the browser
+	}else{
+		xhr = null
+		console.log('ERROR: CORS NOT SUPPORTED IN BROWSER')
+	}
+
+	return xhr
 }
 
-// CORS issue, will resolve tomorrow with XMLHttpRequest (works with IE 11 so that's huge)
-const topPages = 'http://intranet.dvrpc.org/google/analytics?startDate=2017-01-01&endDate=2017-12-01&dimension=ga:pagePath&metric=ga:pageviews,ga:sessions,ga:avgTimeOnPage&sortByMetric=true';
-const check = async () => {
-	const tops = await fetch(topPages, options)
+function getTopFive(url) {
+	const request = createCORSRequest('GET', url)
+
+	// required headers to allow CORS
+	request.setRequestHeader('Access-Control-Allow-Origin', 'http://intranet.dvrpc.org/google/analytics')
+	request.setRequestHeader('Vary', 'Origin')
+
+	if (!getTopFive) throw new Error('CORS not supported')
+
+	request.onload = function() {
+		const text = request.responseText
+		console.log('request text is ', text)
+	}
+
+	request.onerror = function() {
+		console.log('error making the request')
+	}
+
+	request.send()
 }
 
-check()
-
-//const getTopFive = fetch('http://intranet.dvrpc.org/google/analytics?startDate=2017-12-06&endDate=2017-12-06&dimension=ga:hostname&metric=ga:sessions&sortByMetric=true')
+getTopFive(topPages)
